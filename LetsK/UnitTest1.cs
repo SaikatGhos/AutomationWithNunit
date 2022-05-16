cf75dcd32a38f24e9c0810ad25d1d828b7acc3a2
SaikatGhos <91711686+SaikatGhos@users.noreply.github.com>
2022-05-16 10:54:24 +0530

    Commited

--- LetsK/UnitTest1.cs
+++ LetsK/UnitTest1.cs
@@ -1,3 +1,5 @@
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
 using NUnit.Framework;
 using System;
 using TestLib;
@@ -5,16 +7,26 @@ namespace LetsK
 {
     public class Tests:BaseMethods
     {
+        public static ExtentTest Test;
+        public static ExtentReports Extent;
         [SetUp]
         public void Setup()
         {
             driverInitializer();
             GoToURL("https://courses.letskodeit.com/practice");
         }
-
+        [OneTimeSetUp]
+        public void ExtentStart()
+        {
+            Extent = new ExtentReports();
+            var HtmlReporter = new ExtentHtmlReporter(@"C:\Users\SAIKAT\source\repos\TestProject1\LetsK\" + DateTime.Now.ToString("_MMddyyyy_hhmmtt") + ".html");
+            Extent.AttachReporter(HtmlReporter);
+        }
         [Test]
         public void radioButton()
         {
+            Test = null;
+            Test = Extent.CreateTest("T001").Info("RadioButton");
             try
             {
                 Click(propertype.Xpath, "(//*[@id='radio-btn-example']//child::input)[1]");
@@ -22,15 +34,19 @@ namespace LetsK
                 Click(propertype.Xpath, "(//*[@id='radio-btn-example']//child::input)[2]");
                 wait(2000);
                 Click(propertype.Xpath, "(//*[@id='radio-btn-example']//child::input)[3]");
+                Test.Log(Status.Pass, "Test Pass");
             }
             catch(Exception e)
             {
+                Test.Log(Status.Fail, "Test Fail");
                 throw;
             }
         }
         [Test]
         public void selectClassExample()
         {
+            Test = null;
+            Test = Extent.CreateTest("T002").Info("SelectClass");
             try
             {
                 Click(propertype.Xpath, "//*[@id='select-class-example']//child::select//option[1]");
@@ -39,14 +55,18 @@ namespace LetsK
                 wait(2000);
                 Click(propertype.Xpath, "//*[@id='select-class-example']//child::select//option[3]");
                 wait(2000);
+                Test.Log(Status.Pass, "Test Pass");
             }
             catch (Exception e)
             {
+                Test.Log(Status.Fail, "Test Fail");
                 throw;
             }
         }[Test]
         public void mselectClassExample()
         {
+            Test = null;
+            Test = Extent.CreateTest("T003").Info("MSelectClass");
             try
             {
                 Click(propertype.Xpath, "(//*[@id='multiple-select-example']//child::option)[1]");
@@ -55,15 +75,19 @@ namespace LetsK
                 wait(2000);
                 Click(propertype.Xpath, "(//*[@id='multiple-select-example']//child::option)[3]");
                 wait(2000);
+                Test.Log(Status.Pass, "Test Pass");
             }
             catch (Exception e)
             {
+                Test.Log(Status.Fail, "Test Fail");
                 throw;
             }
         }
         [Test]
         public void CheckBox()
         {
+            Test = null;
+            Test = Extent.CreateTest("T004").Info("CheckBox");
             try
             {
                 Click(propertype.Xpath, "(//*[@id='checkbox-example-div']//child::input)[1]");
@@ -72,27 +96,34 @@ namespace LetsK
                 wait(2000);
                 Click(propertype.Xpath, "(//*[@id='checkbox-example-div']//child::input)[3]");
                 wait(2000);
+                Test.Log(Status.Pass, "Test Pass");
             }
             catch (Exception e)
             {
+                Test.Log(Status.Fail, "Test Fail");
                 throw;
             }
         }
         [Test]
         public void mouseHover()
         {
+            Test = null;
+            Test = Extent.CreateTest("T005").Info("MouseHover");
             try
             {
                 MouseHover("//*[@id='mousehover']");
+                Test.Log(Status.Pass, "Test Pass");
             }
             catch (Exception e)
             {
+                Test.Log(Status.Fail, "Test Fail");
                 throw;
             }
         }
         [TearDown]
         public void tearDn()
         {
+            Extent.Flush();
             close();
             quit();
         }
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,58 +1,120 @@
 using AtomationP;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
 using NUnit.Framework;
+using System;
 using TestLib;
 namespace TestProject1
 {
     public class Tests:BaseMethods
     {
+        public static ExtentTest Test;
+        public static ExtentReports Extent;
         [SetUp]
         public void Setup()
         {
             driverInitializer();
             GoToURL("http://automationpractice.com/index.php");
         }
-
+        [OneTimeSetUp]
+        public void ExtentStart()
+        {
+            Extent = new ExtentReports();
+            var HtmlReporter = new ExtentHtmlReporter(@"C:\Users\SAIKAT\source\repos\TestProject1\TestProject1\" + DateTime.Now.ToString("_MMddyyyy_hhmmtt") + ".html");
+            Extent.AttachReporter(HtmlReporter);
+        }
+        
         [Test]
         public void verifyEmailValidorNot()
         {
-
-            invalidEmailCheck.invalidEmail("Abc");
-            wait(5000);
+            Test = null;
+            Test = Extent.CreateTest("T001").Info("verifyEmailValidorNot");
+            try {
+                invalidEmailCheck.invalidEmail("Abc");
+                wait(5000);
+                Test.Log(Status.Pass, "Test Pass");
+            }
+            catch(Exception e) {
+                Test.Log(Status.Fail, "Test Fail");
+                throw; 
+            }
+            
         }
         [Test]
         public void VerifyErrorMessagesforMandatoryFields()
         {
-
-            invalidRegCheck.invalidRegChech();
-            wait(5000);
+            Test = null;
+            Test = Extent.CreateTest("T002").Info("VerifyErrorMessagesforMandatoryFields");
+            try {
+                invalidRegCheck.invalidRegChech();
+                wait(5000);
+                Test.Log(Status.Pass, "Test Pass");
+            }
+            catch(Exception e) {
+                Test.Log(Status.Fail, "Test Fail");
+                throw; }
+            
         }
         [Test]
         public void VerifyErrorMessagesforEnteringIncorrectValuesInFields()
         {
-            invalidRegCheck.invalidDetailsCheck();
-            wait(5000);
+            Test = null;
+            Test = Extent.CreateTest("T003").Info("VerifyErrorMessagesforEnteringIncorrectValuesInFields");
+            try {
+                invalidRegCheck.invalidDetailsCheck();
+                wait(5000);
+                Test.Log(Status.Pass, "Test Pass");
+            } catch (Exception e) {
+                Test.Log(Status.Fail, "Test Fail");
+                throw; }
+            
         }
         [Test]
         public void SearchProduct()
         {
-            productAndFeaturesRelated.search();
-            wait(5000);
+            Test = null;
+            Test = Extent.CreateTest("T004").Info("SearchProduct");
+            try {
+                productAndFeaturesRelated.search();
+                wait(5000);
+                Test.Log(Status.Pass, "Test Pass");
+            } catch (Exception e) {
+                Test.Log(Status.Fail, "Test Fail");
+                throw; }
+            
         }
         [Test]
         public void EndToEndBuy()
         {
-            productAndFeaturesRelated.E2Ebuy();
-            wait(5000);
+            Test = null;
+            Test = Extent.CreateTest("T005").Info("EndToEndBuy");
+            try {
+                productAndFeaturesRelated.E2Ebuy();
+                wait(5000);
+                Test.Log(Status.Pass, "Test Pass");
+            } catch (Exception e) {
+                Test.Log(Status.Fail, "Test Fail");
+                throw; }
+            
         }
         [Test]
         public void AddtoCart()
         {
-            productAndFeaturesRelated.Addto();
-            wait(5000);
+            Test = null;
+            Test = Extent.CreateTest("T006").Info("AddtoCart");
+            try {
+                productAndFeaturesRelated.Addto();
+                wait(5000);
+                Test.Log(Status.Pass, "Test Pass");
+            } catch (Exception e) {
+                Test.Log(Status.Fail, "Test Fail");
+                throw; }
+            
         }
         [TearDown]
         public void tearDn()
         {
+            Extent.Flush();
             close();
             quit();
         }
